bcd5e07891c8293b1e2eea9195307cde315cd6dd
Calle Persson <callepersson94@telia.com>
2017-11-06 10:36:44 +0100

    Unbreaking my day

--- WebServer/Controllers/HomeController.cs
+++ WebServer/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebServer.Controllers
 {
-    [Route("")]
+    [Route("home")]
     public class HomeController : Controller
     {
         public IActionResult Index()
